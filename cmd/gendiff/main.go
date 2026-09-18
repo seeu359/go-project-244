9a6b31d73953d5cf65c443eda1444c3bf6dd2aed
Алексей Черёмушкин <alexey.cheremushkin@uptrade.ru>
2026-09-18 13:52:06 +0300

    step two

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -12,6 +12,14 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "gendiff",
 		Usage: "Compares two configuration files and shows a difference.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "format",
+				Aliases: []string{"f"},
+				Usage:   "output format",
+				Value:   "stylish",
+			},
+		},
 		Action: func(ctx context.Context, cmd *cli.Command) error {
 			return nil
 		},
