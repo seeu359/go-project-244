50b4c66b35f18d7670375a3c42f9034a03711e13
Алексей Черёмушкин <alexey.cheremushkin@uptrade.ru>
2026-09-18 13:49:59 +0300

    step one

---
+++ cmd/gendiff/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "gendiff",
+		Usage: "Compares two configuration files and shows a difference.",
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
